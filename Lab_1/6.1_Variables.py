b8b5a46dd86db6bb58c747b16b26ce2ffd1b9c65
Skabekov Danial <skabekovdanial@gmail.com>
2025-01-17 14:55:32 +0500

    1st Commit

---
+++ Lab_1/6.1_Variables.py
@@ -0,0 +1,12 @@
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+a = 5
+b = "John"
+print(type(x))
+print(type(y))
+
+d = 4
+D = "Sally"
+#A will not overwrite a
---
+++ Lab_1/6.4_Output_variables.py
@@ -0,0 +1,14 @@
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x+y+z)
+#Output: Pythonisawesome
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+#Output: Python is awesome
